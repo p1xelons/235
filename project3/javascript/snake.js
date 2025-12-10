487c6c8957561df7bcf4014cbcc2ed35b5a43960
Sam Hager <93140106+p1xelons@users.noreply.github.com>
2025-12-10 13:31:48 -0500

    getting git back up to date

---
+++ project3/javascript/snake.js
@@ -0,0 +1,15 @@
+import { Application } from 'pixi.js';
+
+// const variables, can change later for better look
+const gridSize = 20;
+const cellSize = 20;
+const gameWidth = gridSize * cellSize;
+const gameHeight = gridSize * cellSize;
+const initialSpeed = 150;
+
+// call pixiJS app
+const app = new __PIXI_APP_INIT__.Application();
+
+(async () => {
+    await app.init
+})
